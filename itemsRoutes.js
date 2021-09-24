f4c18934460d97761d7da4c58229bcc24d040157
Mark <mark@example.dev>
2021-09-23 23:53:07 -0700

    all item routes implemented

--- itemsRoutes.js
+++ itemsRoutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const ExpressError = require('./expressError');
 const router = new express.Router();
 
-function validateJSON(req, res, next){
+function validatePOST(req, res, next){
     const json = req.body;
     const errors = [];
     if (!json.name) errors.push('name');
@@ -19,15 +19,41 @@ function validateJSON(req, res, next){
     return next();
 }
 
+function validateName(req, res, next){
+    if (items.find(elem => elem.name === req.params.name)){
+        return next();
+    } else {
+        return next(new ExpressError(`Item with name '${req.params.name}' does not exist.`, 404))
+    }
+}
+
 // Should return a JSON list of all shopping items
 router.get('/', (req, res) => {
     return res.json(items);
 });
 // Should accept JSON data and add it to the shopping list
-router.post('/', validateJSON, (req, res) => {
+router.post('/', validatePOST, (req, res) => {
     const newItem = {name: req.body.name, price: +(req.body.price.toFixed(2))}
     items.push(newItem);
-    return res.json({'added' : newItem});
+    return res.status(201).json({'added' : newItem});
 });
+// Should display a single item’s name and price
+router.get('/:name', validateName, (req, res) => {
+    const item = items.find(elem => elem.name === req.params.name);
+    return res.json(item);
+});
+// Should modify a single item’s name and/or price.
+router.patch('/:name', validateName, (req, res) => {
+    const item = items.find(elem => elem.name === req.params.name);
+    if (req.body.name) item.name = req.body.name;
+    if (req.body.price) item.price = +req.body.price.toFixed(2);
+    return res.json({'updated': item});
+});
+router.delete('/:name', validateName, (req, res) => {
+    const deleteIdx = items.findIndex(elem => elem.name === req.params.name);
+    items.splice(deleteIdx, 1);
+    return res.json({message: `Deleted ${req.params.name}`});
+})
+
 
 module.exports = router;
